7129cbdf7bc27ae6ae2617337b24867a162c01ba
02.ocampo.edbert@gmail.com <02.ocampo.edbert@gmail.com>
2025-09-22 10:29:26 +0800

    Updated sanity folder

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["edbert-ocampo.vercel.app"],
+    domains: [
+      "edbert-ocampo.vercel.app",
+      "cdn.sanity.io", // 👈 allow Sanity CDN images
+    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "edbert-ocampo.vercel.app",
         pathname: "/_next/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io", // 👈 Sanity CDN
+        pathname: "/images/**",   // Sanity serves images under /images/
       },
     ],
   },
